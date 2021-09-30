e1fe2f6564c77a45236176786d3a18941841e08f
CalabreseAl <CalabreseAl@VT-CALABRESEAL>
2021-09-30 17:03:25 +0200

    Add project files.

---
+++ Homework1_VB/Ball.vb
@@ -0,0 +1,40 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports System.Drawing
+
+Namespace Homework1_VB
+    Public Class Ball
+        Public Property width As Integer
+        Public Property height As Integer
+        Public Property posX As Integer
+        Public Property posY As Integer
+        Public Property speedX As Integer
+        Public Property speedY As Integer
+        Public Property ballColor As Brush
+        Private borderColor As Pen = Pens.Black
+        Private pictureBox As PictureBox
+
+        Public Sub New(ByVal w As Integer, ByVal h As Integer, ByVal x As Integer, ByVal y As Integer, ByVal sX As Integer, ByVal sY As Integer, ByVal color As Brush, ByVal pb As PictureBox)
+            Me.width = w
+            Me.height = h
+            Me.posX = x
+            Me.posY = y
+            Me.speedX = sX
+            Me.speedY = sY
+            Me.ballColor = color
+            Me.pictureBox = pb
+        End Sub
+
+        Public Sub Draw(ByVal g As Graphics)
+            g.FillEllipse(Me.ballColor, Me.posX, Me.posY, Me.width, Me.height)
+            g.DrawEllipse(Me.borderColor, Me.posX, Me.posY, Me.width, Me.height)
+        End Sub
+
+        Public Sub Update()
+            Me.posX += Me.speedX
+            Me.posY += Me.speedY
+            If Me.posX < 0 OrElse Me.posX + Me.width > Me.pictureBox.Width Then Me.speedX = -Me.speedX
+            If Me.posY < 0 OrElse Me.posY + Me.height > Me.pictureBox.Height Then Me.speedY = -Me.speedY
+        End Sub
+    End Class
+End Namespace
---
+++ Homework1_VB/Form1.vb
@@ -0,0 +1,76 @@
+﻿Imports Homework1_VB.Homework1_VB
+
+Public Class Form1
+
+    Public list As ArrayList
+    Private random As Random
+    Private timer As Timer = New Timer()
+
+    Public Sub New()
+        InitializeComponent()
+
+        Me.SetStyle(ControlStyles.AllPaintingInWmPaint Or ControlStyles.UserPaint Or ControlStyles.DoubleBuffer, True)
+        DoubleBuffered = True
+        Me.UpdateStyles()
+
+        random = New Random()
+
+        list = New ArrayList()
+        populate()
+
+        timer.Enabled = True
+        timer.Interval = 20
+        timer.Stop()
+
+        AddHandler timer.Tick, AddressOf Timer1_Tick
+        AddHandler PictureBox1.Paint, AddressOf PictureBox1_Paint
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.timer.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.timer.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        timer.Stop()
+
+        For Each ball As Ball In list
+            ball.posX = random.[Next](0, Me.PictureBox1.Width / 2)
+            ball.posY = random.[Next](0, Me.PictureBox1.Height / 2)
+            ball.speedX = random.[Next](1, 10)
+            ball.speedY = random.[Next](1, 10)
+            ball.ballColor = New SolidBrush(Color.FromArgb(random.[Next](0, 255), random.[Next](0, 255), random.[Next](0, 255)))
+        Next
+
+        timer.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+        Me.PictureBox1.Refresh()
+    End Sub
+
+    Public Sub populate()
+        Dim size As Integer = 50
+
+        For i As Integer = 0 To 10 - 1
+            Dim startX As Integer = random.[Next](0, Me.PictureBox1.Width / 2)
+            Dim startY As Integer = random.[Next](0, Me.PictureBox1.Height / 2)
+            Dim speedX As Integer = random.[Next](1, 10)
+            Dim speedY As Integer = random.[Next](1, 10)
+            Dim ballColor As SolidBrush = New SolidBrush(Color.FromArgb(random.[Next](0, 255), random.[Next](0, 255), random.[Next](0, 255)))
+            list.Add(New Ball(size, size, startX, startY, speedX, speedY, ballColor, Me.PictureBox1))
+        Next
+    End Sub
+
+    Private Sub PictureBox1_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs)
+        Dim g As Graphics = e.Graphics
+
+        For Each ball As Ball In list
+            ball.Update()
+            ball.Draw(g)
+        Next
+    End Sub
+End Class
